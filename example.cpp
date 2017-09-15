e844e3edecc063c9db1a6b7416ac4d8d6feddb51
muralivnv <muralivnv@gmail.com>
2017-09-14 23:34:35 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -14,12 +14,14 @@ int main()
     std::vector<double> v1{1,1,1,1}, v2{1,1,1,1};
     std::array<double, 4> a1{1,1,1,1}, a2{1,1,1,1};
     
-    auto v3 = v1 + v2; // prints {2,2,2,2};
-    auto v4 = v3 + a1;
-    auto a3 = a1 + v4;
-    auto a4 = a2 + a3;
+    auto v3 = v1 + v2; // {2,2,2,2};
+    auto v4 = v3 - a1; // {1,1,1,1};
+    auto a3 = a1 * v4; // {1,1,1,1};
+    auto a4 = a2 / a3; // {1,1,1,1};
     
-    auto result = 2*v4 + a3*25 + a4/2 ;
+    auto result = 2*v4 + a3*25 + a4/2 ; // {27.5, 27.5, 27.5, 27.5};
+ 
+    print_vec(result);
     
     return EXIT_SUCCESS;
 }
